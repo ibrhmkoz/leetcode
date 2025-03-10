68c5b9be5ce404180a16939d9da164ed86ba92c8
ibrahimkoz <ibrahimkoz@outlook.com>
2025-03-10 14:33:26 +0300

    Update ContainsDuplicate.kt

--- ContainsDuplicate.kt
+++ ContainsDuplicate.kt
@@ -1,6 +1,11 @@
 class Solution {
     fun containsDuplicate(nums: IntArray): Boolean {
-        val m = nums.groupBy { it }
-        return m.any { it.value.size > 1 }
+        val m = mutableSetOf<Int>()
+
+        for (n in nums) {
+            if (!m.add(n)) return true
+        }
+
+        return false
     }
 }

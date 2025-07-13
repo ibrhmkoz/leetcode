096328081d691506a59bcd0aa15b2b87927fa0ff
Ibrahim Koz <ibrahimkoz@outlook.com>
2025-07-13 15:12:22 +0300

    Create IsAnagramWithArray.scala

---
+++ ScalaSolutions/IsAnagramWithArray.scala
@@ -0,0 +1,15 @@
+object Solution {
+  def isAnagram(s: String, t: String): Boolean = {
+    val charCount = new Array[Int](26)
+
+    for (c <- s) {
+      charCount(c - 'a') += 1
+    }
+
+    for (c <- t) {
+      charCount(c - 'a') -= 1
+    }
+
+    return charCount.forall(_ == 0)
+  }
+}

0d82a612b6efc14c5195ca0ac8bf148eec6cc84c
Ibrahim Koz <ibrahimkoz@outlook.com>
2025-07-13 23:24:53 +0300

    Update ProductExceptSelf.kt

--- ProductExceptSelf.kt
+++ ProductExceptSelf.kt
@@ -1,19 +1,23 @@
 class Solution {
     fun productExceptSelf(nums: IntArray): IntArray {
-        val res = IntArray(nums.size) { 1 }
-
-        var pre = 1
-        for (r in 0 until nums.lastIndex) {
-            pre *= nums[r]
-            res[r + 1] = pre
+        val result = IntArray(nums.size).apply {
+            this[0] = 1
         }
 
-        var post = 1
-        for (l in nums.lastIndex downTo 1) {
-            post *= nums[l]
-            res[l - 1] *= post
+        for (i in 1..nums.lastIndex) {
+            result[i] = result[i - 1] * nums[i - 1]
         }
 
-        return res
+        // rp[nums.lastIndex] = 1
+        // rp[nums.lastIndex - 1] = nums[lastIndex] * rp[nums.lastIndex] = nums[lastIndex]
+        // rp[nums.lastIndex - 2] = rp[nums.lastIndex - 1] * nums[lastIndex - 1]
+        // tp = rp * lp (tp stands for total product)
+        var rp = nums.last()
+        for (i in nums.lastIndex - 1 downTo 0) {
+            result[i] *= rp
+            rp = rp * nums[i]
+        }
+
+        return result
     }
 }

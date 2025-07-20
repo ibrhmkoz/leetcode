08a72488f94211669c79b84923e5eb9c94b619a5
Ibrahim Koz <ibrahimkoz@outlook.com>
2025-07-20 06:32:51 -0700

    Update MaxRob.kt

--- MaxRob.kt
+++ MaxRob.kt
@@ -2,16 +2,8 @@ class Solution {
     fun rob(nums: IntArray): Int {
         val cache = mutableMapOf<Int, Int>()
         fun maxRob(i: Int): Int {
-            if (i > nums.lastIndex) {
-                return 0
-            }
-
-            return cache.getOrPut(i) {
-                maxOf(
-                    nums[i] + maxRob(i + 2),
-                    maxRob(i + 1)
-                )
-            }
+            if (i > nums.lastIndex) return 0
+            return cache.getOrPut(i) { maxOf(nums[i] + maxRob(i + 2), maxRob(i + 1)) }
         }
 
         return maxRob(0)

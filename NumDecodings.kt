afa667d6bf08b7eddf409ab76488f91b996ad58c
Ibrahim Koz <ibrahimkoz@outlook.com>
2025-07-26 14:23:32 -0700

    Update NumDecodings.kt

--- NumDecodings.kt
+++ NumDecodings.kt
@@ -1,18 +1,13 @@
 class Solution {
     fun numDecodings(s: String): Int {
         val cache = mutableMapOf<Int, Int>()
-        fun read(i: Int): Int {
-            if (i == s.length) return 1
+        fun count(i: Int): Int {
+            if (i > s.lastIndex) return 1
             if (s[i] == '0') return 0
-
-            cache[i]?.let { return it }
-
-            val result =
-                read(i + 1) + if (i + 1 <= s.lastIndex && s.substring(i..i + 1).toInt() <= 26) read(i + 2) else 0
-            cache[i] = result
-            return result
+            return cache.getOrPut(i) {
+                count(i + 1) + if (i < s.lastIndex && s.substring(i, i + 2).toInt() <= 26) count(i + 2) else 0
+            }
         }
-
-        return read(0)
+        return count(0)
     }
 }

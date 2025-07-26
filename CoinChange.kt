b3935dd3b3d14de91238253da766fc74573f9ba8
Ibrahim Koz <ibrahimkoz@outlook.com>
2025-07-26 15:34:57 -0700

    Update CoinChange.kt

--- CoinChange.kt
+++ CoinChange.kt
@@ -1,22 +1,18 @@
 class Solution {
     fun coinChange(coins: IntArray, amount: Int): Int {
         val cache = mutableMapOf<Int, Int>()
-        fun bt(sum: Int): Int {
-            if (sum == 0) return 0
-            else if (sum < 0) return -1
-            cache[sum]?.let { return it }
 
-            val result = coins.asSequence()
-                .map { bt(sum - it) }
-                .filterNot { it == -1 }
-                .minOfOrNull { it + 1 } ?: -1
-
-
-            cache[sum] = result
-
-            return result
+        fun pick(sum: Int): Int = when {
+            sum == 0 -> 0
+            sum < 0 -> -1
+            else -> cache.getOrPut(sum) {
+                coins.asSequence()
+                    .map { pick(sum - it) }
+                    .filterNot { it == -1 }
+                    .minOfOrNull { it + 1 } ?: -1
+            }
         }
 
-        return bt(amount)
+        return pick(amount)
     }
 }

d6269a875290be99f131364149b5ad6c9842e46d
Ibrahim Koz <ibrahimkoz@outlook.com>
2025-08-09 07:55:21 -0700

    Update UniquePaths.kt

--- UniquePaths.kt
+++ UniquePaths.kt
@@ -1,20 +1,12 @@
 class Solution {
     fun uniquePaths(m: Int, n: Int): Int {
         val cache = mutableMapOf<Pair<Int, Int>, Int>()
-        fun f(i: Int, j: Int): Int {
-            if (i < 0 || j < 0) {
-                return 0
-            } else if (i == 0 && j == 0) {
-                return 1
+        fun up(i: Int, j: Int): Int =
+            if (i == 0 || j == 0) 1
+            else cache.getOrPut(i to j) {
+                up(i - 1, j) + up(i, j - 1)
             }
-
-            cache[i to j]?.let { return it }
-
-            val result = f(i - 1, j) + f(i, j - 1)
-            cache[i to j] = result
-            return result
-        }
-
-        return f(m - 1, n - 1)
+        
+        return up(m - 1, n - 1)
     }
 }

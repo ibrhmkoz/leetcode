4ef3902b423c87b69dda3e52944e3b48c8ea4a3c
Ibrahim Koz <ibrahimkoz@outlook.com>
2025-08-23 08:35:43 -0700

    Update WordSearch.kt

--- WordSearch.kt
+++ WordSearch.kt
@@ -6,21 +6,21 @@ class Solution {
             1 to 0,
             -1 to 0
         )
-        val visited = mutableSetOf<Pair<Int, Int>>()
-        
+        val visited = Array(board.size) { BooleanArray(board.first().size) }
+
         fun search(x: Int, y: Int, i: Int): Boolean {
             if (i == word.length) {
                 return true
             }
-            if ((x !in board.indices || y !in board.first().indices) || (x to y in visited)) {
+            if ((x !in board.indices || y !in board.first().indices) || (visited[x][y])) {
                 return false
             }
             if (word[i] != board[x][y]) {
                 return false
             }
 
-            visited.add(x to y)
-            return dirs.any { search(x + it.first, y + it.second, i + 1) }.also { visited.remove(x to y) }
+            visited[x][y] = true
+            return dirs.any { search(x + it.first, y + it.second, i + 1) }.also { visited[x][y] = false }
         }
 
         return board.indices.any { x ->

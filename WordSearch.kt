961396138ada5280b7340da663c298f146739567
Ibrahim Koz <ibrahimkoz@outlook.com>
2025-08-23 08:13:50 -0700

    Create WordSearch.kt

---
+++ WordSearch.kt
@@ -0,0 +1,32 @@
+class Solution {
+    fun exist(board: Array<CharArray>, word: String): Boolean {
+        val dirs = listOf(
+            0 to 1,
+            0 to -1,
+            1 to 0,
+            -1 to 0
+        )
+        val visited = mutableSetOf<Pair<Int, Int>>()
+        
+        fun search(x: Int, y: Int, i: Int): Boolean {
+            if (i == word.length) {
+                return true
+            }
+            if ((x !in board.indices || y !in board.first().indices) || (x to y in visited)) {
+                return false
+            }
+            if (word[i] != board[x][y]) {
+                return false
+            }
+
+            visited.add(x to y)
+            return dirs.any { search(x + it.first, y + it.second, i + 1) }.also { visited.remove(x to y) }
+        }
+
+        return board.indices.any { x ->
+            board.first().indices.any { y ->
+                search(x, y, 0)
+            }
+        }
+    }
+}

fed8ee95b3949483a3c5b5b123bd5583a1a803d6
Ibrahim Koz <ibrahimkoz@outlook.com>
2026-01-12 02:49:03 +0300

    Refactor threeSum function for optimization

--- 3Sum.kt
+++ 3Sum.kt
@@ -1,30 +1,35 @@
 class Solution {
     fun threeSum(nums: IntArray): List<List<Int>> {
+        nums.sort()
         val result = mutableListOf<List<Int>>()
-        val sortedNums = nums.sorted()
-
-        var i = 0
-        while (i <= nums.lastIndex) {
-            var j = i + 1
-            var k = sortedNums.lastIndex
-
-            while (j < k) {
-                val sum = sortedNums[i] + sortedNums[j] + sortedNums[k]
-                if (sum == 0) {
-                    result.add(listOf(sortedNums[i], sortedNums[j], sortedNums[k]))
-                    do {
-                        j++
-                    } while (j < k && sortedNums[j] == sortedNums[j - 1])
-                } else if (sum < 0) {
-                    j++
-                } else {
-                    k--
-                }
+        for (a in 0..nums.lastIndex - 2) {
+            if (nums[a] > 0) {
+                break // Optimization: No more triplets possible
+            }
+            if (a > 0 && nums[a] == nums[a - 1]) {
+                continue
             }
 
-            do {
-                i++
-            } while (i <= nums.lastIndex && sortedNums[i] == sortedNums[i - 1])
+            var b = a + 1
+            var c = nums.lastIndex
+
+            while (b < c) {
+                val sum = nums[a] + nums[b] + nums[c]
+                if (sum == 0) {
+                    result.add(listOf(nums[a], nums[b], nums[c]))
+                    b++
+                    // Any other b that satisfies the sum must have a different c.
+                    // We move both pointers to find the next unique pair.
+                    c--
+
+                    while (b < c && nums[b] == nums[b - 1]) b++
+                    while (b < c && nums[c] == nums[c + 1]) c--
+                } else if (sum > 0) {
+                    c--
+                } else {
+                    b++
+                }
+            }
         }
 
         return result

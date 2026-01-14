62e2439da8ff108d5dbbc5948b8f71eaea82b1f7
Ibrahim Koz <ibrahimkoz@outlook.com>
2026-01-15 00:20:39 +0300

    Refactor characterReplacement method for clarity

--- LongestRepeatingChars.kt
+++ LongestRepeatingChars.kt
@@ -1,25 +1,22 @@
 class Solution {
     fun characterReplacement(s: String, k: Int): Int {
-        val charFrequency = IntArray(26)
+        val freq = IntArray(26)
+
+        var max = 0
         var lo = 0
-        var maxLength = 0
-        var maxFrequent = 0
-        for ((hi, ch) in s.withIndex()) {
-            val hiCharInd = ch - 'A'
-            charFrequency[hiCharInd]++
-            maxFrequent = maxOf(maxFrequent, charFrequency[hiCharInd])
-            val length = hi - lo + 1
-            val freq = length - maxFrequent
-            if (freq > k) {
-                val loCharInd = s[lo] - 'A'
-                charFrequency[loCharInd]--
+        for (hi in s.indices) {
+            freq[s[hi] - 'A']++
+
+            val n = freq.sum() - freq.max()
+            if (k < n) {
+                freq[s[lo] - 'A']--
                 lo++
             }
 
-            val finalLength = hi - lo + 1
-            maxLength = maxOf(maxLength, finalLength)
+            val l = hi - lo + 1
+            max = maxOf(l, max)
         }
 
-        return maxLength
+        return max
     }
 }

07447be5a43de68e2587b40fe69a7049c19138da
Ibrahim Koz <ibrahimkoz@outlook.com>
2026-02-28 15:07:38 +0300

    Replace BoundedPriorityQueue with BoundedArrayDeque

--- DesignTwitter.kt
+++ DesignTwitter.kt
@@ -11,23 +11,33 @@ class BoundedPriorityQueue<T>(
     }
 }
 
+class BoundedArrayDeque<T>(private val bound: Int) : Iterable<T> {
+    private val deque = ArrayDeque<T>()
+
+    fun add(element: T) {
+        if (deque.size >= bound) deque.removeFirst()
+        deque.addLast(element)
+    }
+
+    override fun iterator() = deque.iterator()
+}
+
 data class Tweet(val id: Int, val timestamp: Int, val userId: Int)
 
 class Twitter {
-    private val userTweets = mutableMapOf<Int, BoundedPriorityQueue<Tweet>>()
+    private val userTweets = mutableMapOf<Int, BoundedArrayDeque<Tweet>>()
     private val userFollowees = mutableMapOf<Int, MutableSet<Int>>()
     private val k = 10
     private var timestamp = 0
 
-    private fun tweetQueue() = BoundedPriorityQueue<Tweet>(k) { t1, t2 -> t1.timestamp - t2.timestamp }
 
     fun postTweet(userId: Int, tweetId: Int) {
-        userTweets.getOrPut(userId) { tweetQueue() }.add(Tweet(tweetId, timestamp++, userId))
+        userTweets.getOrPut(userId) { BoundedArrayDeque(k) }.add(Tweet(tweetId, timestamp++, userId))
     }
 
     fun getNewsFeed(userId: Int): List<Int> {
         val followees = userFollowees.getOrPut(userId) { mutableSetOf(userId) }
-        val feed = tweetQueue()
+        val feed = BoundedPriorityQueue<Tweet>(k) { t1, t2 -> t1.timestamp - t2.timestamp }
         followees.asSequence().mapNotNull { userTweets[it] }.flatMap { it }.toCollection(feed)
         return feed.sortedByDescending { it.timestamp }.map { it.id }
     }

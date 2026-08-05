ba358aa0c7e762f527af9dd15737c8c4c24039d1
Ibrahim Koz <ibrahimkoz@outlook.com>
2026-08-05 14:05:13 +0300

    Refactor lengthOfLIS function for clarity and efficiency

--- LengthOfLIS.kt
+++ LengthOfLIS.kt
@@ -1,17 +1,13 @@
 class Solution {
     fun lengthOfLIS(nums: IntArray): Int {
-        val cache = mutableMapOf(
-            nums.lastIndex to 1
-        )
-
-        fun lisFromIndex(i: Int): Int = cache.getOrPut(i) {
-            (i + 1..nums.lastIndex).filter { nums[it] > nums[i] }.maxOfOrNull { 1 + lisFromIndex(it) } ?: 1
-        }
-
-        for (i in nums.indices) {
-            lisFromIndex(i)
-        }
-
-        return cache.values.max()
+        val cache = mutableMapOf(nums.lastIndex to 1)
+        fun lis(i: Int): Int =
+            cache.getOrPut(i) {
+                (i + 1..nums.lastIndex)
+                    .filter { nums[it] > nums[i] }
+                    .maxOfOrNull { 1 + lis(it) } ?: 1
+            }
+        
+        return nums.indices.maxOfOrNull { lis(it) } ?: 0
     }
 }

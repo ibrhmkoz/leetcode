b9e8e4a9f861788ca947e99de8941f3a4cdb3488
Ibrahim Koz <ibrahimkoz@outlook.com>
2026-08-07 13:19:17 +0300

    Refactor merge function for interval merging

--- MergeIntervals.kt
+++ MergeIntervals.kt
@@ -1,32 +1,15 @@
 class Solution {
     fun merge(intervals: Array<IntArray>): Array<IntArray> {
-        if (intervals.isEmpty()) return emptyArray()
-
-        intervals.sortWith(compareBy<IntArray> { it[0] }.thenBy { it[1] })
-
-        val newIntervals = mutableListOf(
-            intervals.first()
-        )
-
-        fun merge(i1: IntArray, i2: IntArray): IntArray? {
-            if (i2[0] <= i1[1]) {
-                return intArrayOf(i1[0], maxOf(i1[1], i2[1]))
-            }
-            return null
-        }
-
-        for (interval in intervals.slice(1..intervals.lastIndex)) {
-            val lastEl = newIntervals.removeLast()
-
-            val merged = merge(lastEl, interval)
-            if (merged != null) {
-                newIntervals.add(merged)
+        intervals.sortBy { it[0] }
+        val result = mutableListOf<IntArray>()
+        for (iv in intervals) {
+            val last = result.lastOrNull()
+            if (last != null && last[1] >= iv[0]) {
+                last[1] = maxOf(last[1], iv[1])
             } else {
-                newIntervals.add(lastEl)
-                newIntervals.add(interval)
+                result.add(iv)
             }
         }
-
-        return newIntervals.toTypedArray()
+        return result.toTypedArray()
     }
 }
